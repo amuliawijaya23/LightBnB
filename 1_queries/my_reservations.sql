afb79a323335c85cf2425ff1d423f2bbaf419e01
amuliawijaya23 <amuliawijaya@gmail.com>
2022-04-06 22:22:20 -0700

    re-construct table

--- 1_queries/my_reservations.sql
+++ 1_queries/my_reservations.sql
@@ -2,7 +2,7 @@ SELECT reservations.id, properties.title, properties.cost_per_night, start_date,
 FROM reservations
 JOIN properties on properties.id = reservations.property_id
 JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE reservations.guest_id = 7
+WHERE reservations.guest_id = 4
 GROUP BY reservations.id, properties.id
 ORDER BY start_date
 LIMIT 10;
